de40580d17e12f114b3cdd8f1b49b4494292d157
vkfbsrnd <101619604+vkfbsrnd@users.noreply.github.com>
2022-04-12 16:20:59 +0900

    알파카
    ㅇㅇㅇㅇㅇ

---
+++ main.js
@@ -0,0 +1,18 @@
+const circle=document.querySelector(".circle");
+const HL=document.querySelector(".horizontal_line");
+const VL=document.querySelector(".vertical_line");
+document.addEventListener("mousemove",(e)=>{
+    console.log(e.clientX);
+    console.log(e.clientY);
+    const mouseX= e.clientX;
+    const mouseY= e.clientY;
+    circle.style.top= mouseY+'px';
+    circle.style.left= mouseX+'px';
+    HL.style.top= mouseY+'px';
+    VL.style.left= mouseX+'px';
+    var XY = mouseX + "," + mouseY;
+    document.getElementById("result").innerHTML=XY;
+    document.getElementById("result").style.top= mouseY+'px';
+    document.getElementById("result").style.left= mouseX+'px';
+
+})
